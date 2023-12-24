97721c441a8cdcf820f67753f4fafa6d3841a5cf
Lynn Langit <lynnlangit@live.com>
2023-12-24 08:50:27 -0600

    Update LSAPI-job.sh

--- 7_samtools_deployments/scripts/LSAPI-job.sh
+++ 7_samtools_deployments/scripts/LSAPI-job.sh
@@ -3,4 +3,4 @@ gcloud beta lifesciences pipelines run \
     --command-line 'samtools index ${BAM} ${BAI}' \
     --docker-image "gcr.io/cloud-lifesciences/samtools" \
     --inputs BAM=gs://genomics-public-data/NA12878.chr20.sample.bam \
-    --outputs BAI=gs://mayo-demos-life-sciences/NA12878.chr20.sample.bam.bai
+    --outputs BAI=gs://samtools-langit/BAM/NA12878.chr20.sample.bam.bai
