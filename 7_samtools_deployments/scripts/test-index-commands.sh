4bc334e6bd4546736da2452efd67df57bc382bc1
Lynn Langit <lynnlangit@live.com>
2023-12-24 08:28:20 -0600

    Create test-index-commands.sh

---
+++ 7_samtools_deployments/scripts/test-index-commands.sh
@@ -0,0 +1,13 @@
+# put samtools in path
+export PATH=/home/username/samtools/bin:$PATH
+
+# verify install
+samtools --version
+
+# get sample BAM file
+wget http://hgdownload.cse.ucsc.edu/goldenPath/hg19/encodeDCC/wgEncodeUwRepliSeq/wgEncodeUwRepliSeqBg02esG1bAlnRep1.bam
+
+# test samtools with basic commands
+samtools index wgEncodeUwRepliSeqBg02esG1bAlnRep1.bam
+samtools view wgEncodeUwRepliSeqBg02esG1bAlnRep1.bam | head
+samtools flagstat wgEncodeUwRepliSeqBg02esG1bAlnRep1.bam
